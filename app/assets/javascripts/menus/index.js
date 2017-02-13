7592473fe420e24ad338223fbbd97a2ade65f6cb
Olena_Ch <master_of_time@bigmir.net>
2017-02-13 20:32:58 +0000

    CSS refactoring

--- app/assets/javascripts/menus/index.js
+++ app/assets/javascripts/menus/index.js
@@ -1,79 +1,35 @@
 $(document).ready(function() {
-  $(document).on('click', '.choose', function() {
-    setInputIdentifiers();
-    setButtonsIdentifiers();
-    openItemsModal(this);
-    setPageVariables(this);
-  });
-  
-  $('.item').click(function() {
-    var slctr = '#menu-item-' + field_id;
-    $(slctr).attr('value', $(this).attr('data'));
-    hidePopups();
-  });
-  
-  $('.btn.add-item-to-menu').click(function() {
-    var html = `
-      <div class='course-row'>
-        <label class='item-label menu-new'>Menu item:</label>
-          <div class='item-input menu-new'>
-            <input type='text' class='input menu-item' name='menu[items][]' id='menu' value="">
-            <div class='menu-new item-buttons'>
-              <div class='choose f-menu-item' data='nil'><p>F</p></div>
-              <div class='choose m-menu-item' data='nil'><p>M</p></div>
-              <div class='choose d-menu-item' data='nil'><p>D</p></div>
-            </div>
-          </div>
-      </div>
-    `;
-    $('.form-element.courses').append(html);
-  });
-  
-  $('.first-items-close, .main-items-close, .drinks-close').click(function() {
-    hidePopups();
-  });
-  
   $('.shopping-cart-img').click(function() {
     enlargeOrderVariables(this);
-    formOrderPreview();
-    countTotalCalories();
-    countTotalSum();
-    setNewOrderParams();
-    $('.sidebar-item.order-preview-form').removeClass('hidden');
+    prepareOrderPreview();
   });
   
   $('.preview-img').click(function() {
-    $('.menu-item-details').show();
+    $('.weekday-menu-item-details').show();
   });
   
-  $('.menu-item-details-close').click(function() {
-    $('.menu-item-details').hide();
+  $('.weekday-menu-item-details-close').click(function() {
+    $('.weekday-menu-item-details').hide();
   });
   
   $('.order-preview-form').click(function() {
-    $('.popup-order-preview').show();
+    $('.popup.order-preview').show();
   });
   
-  $('.order-preview-close').click(function() {
-    $('.popup-order-preview').hide();
+  $('.popup-close.order-preview').click(function() {
+    $('.popup.order-preview').hide();
   });
   
   $('.remove-item-from-order').click(function() {
-    var shown_items = $('.popup-order-preview').find('.order-preview-item.flex');
-    for(var i = 0, size = shown_items.length; i < size; i++) {
-      if($(this).attr('data-id') == $(shown_items[i]).attr('data')) {
-        $(shown_items[i]).removeClass('flex');
-        $(shown_items[i]).addClass('hidden');
-      }
-    }
+    removeChosenItem(this);
     lessenOrderVariables(this);
     countTotalCalories();
     countTotalSum();
     setNewOrderParams();
   });
   
-  $('.menus-list-item.small').click(function() {
-    var menu_list = $('.menus-list').find('.hidden-data');
+  $('.weekday-menu-item.small-img').click(function() {
+    var menu_list = $('.weekday-menu-items').find('.hidden-data');
     for (var i = 0, size = menu_list.length; i < size; i++) {
       if ($(this).attr('data') == $(menu_list[i]).attr('data')) {
         $(menu_list[i]).removeClass('hidden-data');
@@ -82,15 +38,14 @@ $(document).ready(function() {
     }
   });
   
-  $('.menus-list-item-enlarged-close').click(function() {
-    var menu_list = $('.menus-list').find('.shown-data');
+  $('.weekday-menu-item.enlarged-img.close').click(function() {
+    var menu_list = $('.weekday-menu-items').find('.shown-data');
     for (var i = 0, size = menu_list.length; i < size; i++) {
       $(menu_list[i]).removeClass('shown-data');
       $(menu_list[i]).addClass('hidden-data');
     }
   });
-
-  var field_id;
+  
   var ordered_meals = [];
   var order_sum = 0;
   var order_calories = 0;
@@ -112,60 +67,42 @@ $(document).ready(function() {
   };
   
   var formOrderPreview = function() {
-    var order_items = $('.popup-order-preview').find('.order-preview-item.hidden');
-    for(var i = 0, size = order_items.length; i < size; i++) {
+    var order_items = $('.popup.order-preview').find('.order-preview-item.hidden');
+    for(var i = 0, size = order_items.length; i < size; i++)
       if(ordered_meals.includes($(order_items[i]).attr('data'))) {
         $(order_items[i]).removeClass('hidden');
         $(order_items[i]).addClass('flex');
       }
-    }
-  };
-  
-  var hidePopups = function() {
-    $('.popup-items').hide();
   };
   
   var lessenOrderVariables = function(obj) {
     var ind = ordered_meals.indexOf($(obj).attr('data-id'));
-    if(ind != -1) {
+    if(ind != -1)
     	ordered_meals.splice(ind, 1);
-    }
     order_sum -= $(obj).attr('data-price');
     order_calories -= $(obj).attr('data-calories');
-    console.log(ordered_meals);
   };
   
-  var openItemsModal = function(obj) {
-    if ($(obj).attr('class').includes('f-menu-item'))
-      $('.popup-items.first-course').show();
-    if ($(obj).attr('class').includes('m-menu-item'))
-      $('.popup-items.main-course').show();
-    if ($(obj).attr('class').includes('d-menu-item'))
-      $('.popup-items.drink').show();
+  var prepareOrderPreview = function() {
+    formOrderPreview();
+    countTotalCalories();
+    countTotalSum();
+    setNewOrderParams();
+    $('.sidebar-item.order-preview-form').removeClass('hidden');
   };
-
-  var setButtonsIdentifiers = function() {
-    var menu_items = $('form').find('.course-row');
-    for(var i = 0, size = menu_items.size(); i < size; i++) {
-      var choose_btns = $(menu_items[i]).find('.choose');
-      for(var j = 0; j < 3; j++) { 
-        $(choose_btns[j]).attr('data', i);
+  
+  var removeChosenItem = function(obj) {
+    var shown_items = $('.popup.order-preview').find('.order-preview-item.flex');
+    for(var i = 0, size = shown_items.length; i < size; i++) {
+      if($(obj).attr('data-id') == $(shown_items[i]).attr('data')) {
+        $(shown_items[i]).removeClass('flex');
+        $(shown_items[i]).addClass('hidden');
       }
     }
   };
-  
-  var setInputIdentifiers = function() {
-    var menu_items = $('form').find('.input.menu-item');
-    for(var i = 0, size = menu_items.size(); i < size; i++) {
-      $(menu_items[i]).attr('id', 'menu-item-' + i);
-    }
-  };
-  
-  var setPageVariables = function(obj) {
-    field_id = $(obj).attr('data');
-  };
-  
+
   var setNewOrderParams = function() {
-    $('.place-order-href').attr('href', '/orders/new?items_ids=' + ordered_meals.join(','));
+    var link = '/orders/new?items_ids=' + ordered_meals.join(',');
+    $('.place-order-href').attr('href', link);
   };
 });
---
+++ app/assets/javascripts/menus/new_edit_page.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+  $(document).on('click', '.choose', function() {
+    setInputIdentifiers();
+    setButtonsIdentifiers();
+    openItemsModal(this);
+    setPageVariables(this);
+  });
+  
+  $('.menu.item').click(function() {
+    var slctr = '#menu-item-' + field_id;
+    $(slctr).attr('value', $(this).attr('data'));
+    hidePopups();
+  });
+  
+  $('.btn.add-item-to-menu').click(function() {
+    var html = `
+      <div class='course flex'>
+        <label class='menu form-element label'>Menu item:</label>
+        <div class='menu form-element input flex'>
+          <input type='text' class='menu item form-element input' name='menu[items][]' id='menu'>
+          <div class='menu item-buttons flex'>
+            <div class='choose f-menu-item' data='nil'><p>F</p></div>
+            <div class='choose m-menu-item' data='nil'><p>M</p></div>
+            <div class='choose d-menu-item' data='nil'><p>D</p></div>
+          </div>
+        </div>
+      </div>
+    `;
+    $('.menu.form-element.courses').append(html);
+  });
+  
+  $('.popup-close.first-course, .popup-close.main-course, .popup-close.drink'
+    ).click(function() {
+      hidePopups();
+  });
+  
+  var field_id;
+  
+  var hidePopups = function() {
+    $('.popup').hide();
+  };
+  
+  var openItemsModal = function(obj) {
+    if ($(obj).attr('class').includes('f-menu-item'))
+      $('.popup.first-course').show();
+    if ($(obj).attr('class').includes('m-menu-item'))
+      $('.popup.main-course').show();
+    if ($(obj).attr('class').includes('d-menu-item'))
+      $('.popup.drink').show();
+  };
+
+  var setButtonsIdentifiers = function() {
+    var menu_items = $('form').find('.course');
+    for(var i = 0, size = menu_items.size(); i < size; i++) {
+      var choose_btns = $(menu_items[i]).find('.choose');
+      for(var j = 0; j < 3; j++) { $(choose_btns[j]).attr('data', i); }
+    }
+  };
+  
+  var setInputIdentifiers = function() {
+    var menu_items = $('form').find('.menu.item.form-element.input');
+    for(var i = 0, size = menu_items.size(); i < size; i++) {
+      $(menu_items[i]).attr('id', 'menu-item-' + i);
+    }
+  };
+  
+  var setPageVariables = function(obj) {
+    field_id = $(obj).attr('data');
+  };
+});
