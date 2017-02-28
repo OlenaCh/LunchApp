f59f27bbe11b3610a911d31d117f15d1f18a8a0b
Olena_Ch <master_of_time@bigmir.net>
2017-02-28 01:57:19 +0000

    RSpec for helper 1.0

---
+++ spec/helpers/menus_helper_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe MenusHelper, type: :helper do
+  it 'returns true if weekday is today' do
+    #
+  end
+  
+  it 'returns false if weekday is not today' do
+    #
+  end
+end
